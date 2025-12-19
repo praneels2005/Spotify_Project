bd10c19fd0a62dc73c7eed3abf31722628dcce86
praneels2005 <praneels2005@gmail.com>
2025-12-19 17:25:36 -0500

    "Optimized the UI_Front directory by removing the jam-genie folder"

--- UI_Front/jam-genie/src/config/spotify.ts
+++
@@ -1,6 +0,0 @@
-export const CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID ?? "";
-export const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
-export const REDIRECT_URL_AFTER_LOGIN = import.meta.env.VITE_SPOTIFY_REDIRECT_URI ?? "http://localhost:5000/callback";
-export const scope = "user-read-email playlist-modify-public playlist-modify-private";
-export const responseType = "code";
-export const showDialog = "true";
--- UI_Front/jam-genie/src/pages/Index.tsx
+++
@@ -1,7 +0,0 @@
-import SpotifyPlaylistGenerator from "@/components/SpotifyPlaylistGenerator";
-
-const Index = () => {
-  return <SpotifyPlaylistGenerator />;
-};
-
-export default Index;
